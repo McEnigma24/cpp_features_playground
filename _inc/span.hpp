c906bc41301ac741ae3d3bad3df78a937d0581f8
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-04-04 01:13:42 +0200

    quick

--- _inc/span.hpp
+++ _inc/span.hpp
@@ -1,7 +1,7 @@
 #include "__preprocessor__.h"
 #include <span>
 
-void span_play()
+void simple_array()
 {
     std::array<int, 5> arr = {1, 2, 3, 4, 5};
     std::span<int> sp(arr.data(), arr.size() - 2);
@@ -24,3 +24,75 @@ void span_play()
     }
     std::cout << std::endl;
 }
+
+struct AaSysComMsg
+{
+    int id;
+    size_t payload_size;
+    void* payload_ptr;
+
+public:
+    AaSysComMsg(int _id, size_t _payload_size) : id(_id), payload_size(_payload_size) { payload_ptr = malloc(payload_size); }
+
+    ~AaSysComMsg()
+    {
+        if (payload_ptr)
+        {
+            free(payload_ptr);
+            payload_ptr = nullptr;
+        }
+    }
+
+    void* get_payload() { return payload_ptr; }
+};
+
+struct AaSysCom
+{
+    static void* msg_create(span<u8>& payload, int id, size_t payload_size)
+    {
+        // można też tutaj dać malloc, wypełnić ręcznie i zrobić drugi alloc
+        // ale od tego jest C++, żeby to zrobił automatycznie
+
+        AaSysComMsg* msg = new AaSysComMsg(id, payload_size);
+        payload = span<u8>(static_cast<u8*>(msg->get_payload()), payload_size);
+
+        return msg;
+    }
+    static void msg_send(void* msg)
+    {
+        AaSysComMsg* sys_msg = reinterpret_cast<AaSysComMsg*>(msg);
+
+        cout << "Sending message with ID: " << sys_msg->id << "\n";
+        cout << "Sending message with ID: " << sys_msg->payload_size << "\n";
+
+        delete msg;
+    }
+};
+
+struct DiscoveryMsg
+{
+    static int id;
+    static size_t size;
+    static void* create(span<u8>& msg_payload) { return AaSysCom::msg_create(msg_payload, id, size); }
+
+public:
+    int a;
+    float b;
+};
+
+int DiscoveryMsg::id = 0x1234;
+size_t DiscoveryMsg::size = sizeof(DiscoveryMsg);
+
+void dynamic_memory()
+{
+    span<u8> msg_payload;
+    void* msg = DiscoveryMsg::create(msg_payload);
+
+    auto& discoveryMsg = *(reinterpret_cast<DiscoveryMsg*>(msg_payload.data()));
+    discoveryMsg.a = 42;
+    discoveryMsg.b = 3.14f;
+
+    AaSysCom::msg_send(msg);
+}
+
+void span_play() { dynamic_memory(); }
