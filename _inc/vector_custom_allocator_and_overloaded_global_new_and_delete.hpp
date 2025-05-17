6d0a4b4f0d6bbcae7471c4d6aa9d9438ef4abf1c
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-05-17 14:44:06 +0200

    quick

--- _inc/vector_custom_allocator_and_overloaded_global_new_and_delete.hpp
+++ _inc/vector_custom_allocator_and_overloaded_global_new_and_delete.hpp
@@ -2,7 +2,7 @@
 
 void* operator new(size_t size)
 {
-    cout << "New operator overloading " << endl;
+    cout << "New operator overloading - " << size << endl;
     void* p = malloc(size);
     return p;
 }
@@ -43,9 +43,6 @@ void checking_if_it_works()
     delete paaaaaaa;
 }
 
-void zuza(int liczba_calkowita) { int wiek_zuzy = liczba_calkowita; }
-
-// Custom allocator that uses a pre-allocated buffer
 template <typename T>
 class BufferAllocator
 {
@@ -53,43 +50,20 @@ public:
     using value_type = T;
 
     // Constructor takes existing buffer and its capacity (in elements)
-    BufferAllocator(T* buffer_ptr, std::size_t capacity) : buffer(buffer_ptr), capacity(capacity), offset(0) {}
-
-    // Copy constructor template for rebinding
-    template <typename U>
-    BufferAllocator(const BufferAllocator<U>& other) noexcept : buffer(other.buffer), capacity(other.capacity), offset(other.offset)
-    {
-    }
+    BufferAllocator(T* buffer, size_t capacity) : buffer(buffer), capacity(capacity) {}
 
     // Allocate n elements from the buffer
-    T* allocate(std::size_t n)
+    T* allocate(size_t n)
     {
-        if (offset + n > capacity) { throw std::bad_alloc(); }
-        T* ptr = buffer + offset;
-        offset += n;
-        return ptr;
+        if (n > capacity) { throw bad_alloc(); }
+        return buffer;
     }
 
-    // Deallocate does nothing (buffer persists)
-    void deallocate(T* /*p*/, std::size_t /*n*/) noexcept
-    {
-        // no-op
-    }
-
-    // Allocator traits
-    template <typename U>
-    struct rebind
-    {
-        using other = BufferAllocator<U>;
-    };
+    void deallocate(T* /*p*/, size_t /*n*/) noexcept { delete[] buffer; }
 
 private:
-    template <typename U>
-    friend class BufferAllocator;
-
     T* buffer;
-    std::size_t capacity;
-    std::size_t offset;
+    size_t capacity;
 };
 
 void vector_custom_allocator_and_overloaded_global_new_and_delete()
@@ -97,36 +71,18 @@ void vector_custom_allocator_and_overloaded_global_new_and_delete()
     time_stamp("vector_custom_allocator_and_overloaded_global_new_and_delete");
     // checking_if_it_works();
 
+    const size_t BUFFER_SIZE = 100000000;
+
     for (;;)
     {
-        const size_t BUFFER_SIZE = 100000000;
+        int* buffer = new int[BUFFER_SIZE];
 
-        vector<int> vec(BUFFER_SIZE);
-        memset(vec.data(), 66, BUFFER_SIZE * sizeof(int));
-        var(vec[0]);
+        // Create vector using our custom allocator //
+
+        //                                        n                                       capacity
+        vector<int, BufferAllocator<int>> vec(BUFFER_SIZE, BufferAllocator<int>(buffer, BUFFER_SIZE));
+
+        memset(vec.data(), -1, BUFFER_SIZE * sizeof(int));
+        memset(buffer, -1, BUFFER_SIZE * sizeof(int));
     }
-
-    // int main() {
-    //     constexpr std::size_t BUFFER_SIZE = 100;
-    //     // Pre-allocated storage for 100 ints
-    //     static int preallocated_buffer[BUFFER_SIZE];
-
-    //     // Create vector using our custom allocator
-    //     std::vector<int, BufferAllocator<int>> vec(
-    //         BufferAllocator<int>(preallocated_buffer, BUFFER_SIZE)
-    //     );
-
-    //     // Use the vector as usual
-    //     for (int i = 0; i < 10; ++i) {
-    //         vec.push_back(i * i);
-    //     }
-
-    //     // Output contents
-    //     for (int x : vec) {
-    //         std::cout << x << " ";
-    //     }
-    //     std::cout << std::endl;
-
-    //     return 0;
-    // }
 }
