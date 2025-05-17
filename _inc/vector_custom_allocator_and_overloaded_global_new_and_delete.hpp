9b576b397e440daa9c030a6f9ed7e5e11e28b912
McEnigma24 <wojciech.mackowiak24@gmail.com>
2025-05-17 13:14:19 +0200

    quick

--- _inc/vector_custom_allocator_and_overloaded_global_new_and_delete.hpp
+++ _inc/vector_custom_allocator_and_overloaded_global_new_and_delete.hpp
@@ -43,53 +43,68 @@ void checking_if_it_works()
     delete paaaaaaa;
 }
 
+void zuza(int liczba_calkowita) { int wiek_zuzy = liczba_calkowita; }
+
+// Custom allocator that uses a pre-allocated buffer
+template <typename T>
+class BufferAllocator
+{
+public:
+    using value_type = T;
+
+    // Constructor takes existing buffer and its capacity (in elements)
+    BufferAllocator(T* buffer_ptr, std::size_t capacity) : buffer(buffer_ptr), capacity(capacity), offset(0) {}
+
+    // Copy constructor template for rebinding
+    template <typename U>
+    BufferAllocator(const BufferAllocator<U>& other) noexcept : buffer(other.buffer), capacity(other.capacity), offset(other.offset)
+    {
+    }
+
+    // Allocate n elements from the buffer
+    T* allocate(std::size_t n)
+    {
+        if (offset + n > capacity) { throw std::bad_alloc(); }
+        T* ptr = buffer + offset;
+        offset += n;
+        return ptr;
+    }
+
+    // Deallocate does nothing (buffer persists)
+    void deallocate(T* /*p*/, std::size_t /*n*/) noexcept
+    {
+        // no-op
+    }
+
+    // Allocator traits
+    template <typename U>
+    struct rebind
+    {
+        using other = BufferAllocator<U>;
+    };
+
+private:
+    template <typename U>
+    friend class BufferAllocator;
+
+    T* buffer;
+    std::size_t capacity;
+    std::size_t offset;
+};
+
 void vector_custom_allocator_and_overloaded_global_new_and_delete()
 {
     time_stamp("vector_custom_allocator_and_overloaded_global_new_and_delete");
-    checking_if_it_works();
+    // checking_if_it_works();
 
-    //     // Custom allocator that uses a pre-allocated buffer
-    // template <typename T>
-    // class BufferAllocator {
-    // public:
-    //     using value_type = T;
+    for (;;)
+    {
+        const size_t BUFFER_SIZE = 100000000;
 
-    //     // Constructor takes existing buffer and its capacity (in elements)
-    //     BufferAllocator(T* buffer_ptr, std::size_t capacity)
-    //         : buffer(buffer_ptr), capacity(capacity), offset(0) {}
-
-    //     // Copy constructor template for rebinding
-    //     template <typename U>
-    //     BufferAllocator(const BufferAllocator<U>& other) noexcept
-    //         : buffer(other.buffer), capacity(other.capacity), offset(other.offset) {}
-
-    //     // Allocate n elements from the buffer
-    //     T* allocate(std::size_t n) {
-    //         if (offset + n > capacity) {
-    //             throw std::bad_alloc();
-    //         }
-    //         T* ptr = buffer + offset;
-    //         offset += n;
-    //         return ptr;
-    //     }
-
-    //     // Deallocate does nothing (buffer persists)
-    //     void deallocate(T* /*p*/, std::size_t /*n*/) noexcept {
-    //         // no-op
-    //     }
-
-    //     // Allocator traits
-    //     template <typename U>
-    //     struct rebind { using other = BufferAllocator<U>; };
-
-    // private:
-    //     template <typename U>
-    //     friend class BufferAllocator;
-
-    //     T* buffer;
-    //     std::size_t capacity;
-    //     std::size_t offset;
-    // };
+        vector<int> vec(BUFFER_SIZE);
+        memset(vec.data(), 66, BUFFER_SIZE * sizeof(int));
+        var(vec[0]);
+    }
 
     // int main() {
     //     constexpr std::size_t BUFFER_SIZE = 100;
